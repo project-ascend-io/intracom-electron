fb2e37d276b922b1e368c36d31abe4abaf80834f
Tolulope Olobayo <tolobayo1@gmail.com>
2024-07-23 17:37:19 -0400

    feat(input): utitlize react-hook-form

--- src/Components/input/input.tsx
+++ src/Components/input/input.tsx
@@ -1,31 +1,31 @@
-import React, { ChangeEvent, FC, InputHTMLAttributes } from "react";
+import React, { FC, InputHTMLAttributes } from "react";
 import toTitleCase from "../../utils/titlecase/titlecase";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeholder?: string;
-  value: string;
   type: string;
-  handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  // these props are used in conjunction with react-hook-form
+  register: any;
+  errors: any;
 }
 const Input: FC<InputProps> = ({
   name,
-  value,
-  handleChange,
+  register,
   placeholder,
   type,
+  errors,
 }) => {
   return (
     <div className="input-group">
       <label htmlFor={name}>{toTitleCase(name)}</label>
       <input
         id={name}
-        name={name}
         type={type}
-        onChange={handleChange}
-        value={value}
+        {...register(name)}
         placeholder={placeholder}
       />
+      {errors[name] && <span>{errors[name]?.message}</span>}
     </div>
   );
 };
