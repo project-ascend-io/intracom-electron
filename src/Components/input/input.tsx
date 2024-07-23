51f465719b37630ab17713616a615402fd8392bc
Tolulope Olobayo <tolobayo1@gmail.com>
2024-07-23 17:01:51 -0400

    fix(input): add type prop to assign input type i.e. email, password etc

--- src/Components/input/input.tsx
+++ src/Components/input/input.tsx
@@ -5,16 +5,23 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   placeholder?: string;
   value: string;
+  type: string;
   handleChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
-const Input: FC<InputProps> = ({ name, value, handleChange, placeholder }) => {
+const Input: FC<InputProps> = ({
+  name,
+  value,
+  handleChange,
+  placeholder,
+  type,
+}) => {
   return (
     <div className="input-group">
       <label htmlFor={name}>{toTitleCase(name)}</label>
       <input
         id={name}
         name={name}
-        type="text"
+        type={type}
         onChange={handleChange}
         value={value}
         placeholder={placeholder}
