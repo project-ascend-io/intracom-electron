63fdad79c667e04970417aeecad877fc1fd3be2c
Tolulope Olobayo <tolobayo1@gmail.com>
2024-07-23 11:50:33 -0400

    feat: create auth context

---
+++ src/context/auth-context.tsx
@@ -0,0 +1,30 @@
+import React, { useContext, createContext, useState } from "react";
+
+type AuthContextType = {
+  user: any;
+  setUser: any;
+};
+type AuthProviderType = {
+  children: React.ReactNode;
+};
+type AuthUserType = {
+  name: string;
+  email: string;
+};
+
+// stores auth context values
+const AuthContext = createContext({} as AuthContextType);
+
+export const AuthProvider = ({ children }: AuthProviderType) => {
+  const [user, setUser] = useState<AuthUserType | null>(null);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// auth consumer hook - this is what will be imported to utilize the user object and set user function
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
