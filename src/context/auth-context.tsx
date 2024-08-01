b2351596e4e944e75aa43f561a9d6872fa564a4c
Tolulope Olobayo <tolobayo1@gmail.com>
2024-08-01 17:26:44 -0400

    fix(auth context): move auth types to types file

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,22 +1,12 @@
 import React, { useContext, createContext, useState } from "react";
-
-type AuthContextType = {
-  user: any;
-  setUser: any;
-};
-type AuthProviderType = {
-  children: React.ReactNode;
-};
-// TODO: Update this with expected values sent from backend
-type AuthUserType = {
-  [key: string]: any;
-};
+import { AuthContextType, AuthUserType, AuthProviderType } from "../types/auth";
 
 // stores auth context values
 const AuthContext = createContext({} as AuthContextType);
 
 export const AuthProvider = ({ children }: AuthProviderType) => {
   const [user, setUser] = useState<AuthUserType | null>(null);
+
   return (
     <AuthContext.Provider value={{ user, setUser }}>
       {children}
