67def12f59d8e1e793856af407e266585efbcbdb
Tolulope Olobayo <tolobayo1@gmail.com>
2024-08-02 21:34:56 -0400

    fix(route protection): update logic to properly handle asynchronous data

--- src/Components/auth-required/auth-required.tsx
+++ src/Components/auth-required/auth-required.tsx
@@ -1,8 +1,25 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../../context/auth-context";
+import { useEffect, useState } from "react";
+import { checkUser } from "../../services/auth";
+import { ResponseObject } from "../../types/auth";
 const AuthRequired = () => {
-  const { user } = useAuth();
-  return user ? <Outlet /> : <Navigate to="/login" />;
+  const { user, setUser } = useAuth();
+  //Note: I created this loading state to trigger updated render once the fetch is complete. You have to include a state value (instead of just relying on the user value above) because it syncs with the component.
+  const [loading, setLoading] = useState(true);
+  const getUserData = async () => {
+    //check for active session
+    const data = await checkUser();
+    //TODO: add some global error handling within context to display auth errors
+    if (data?.success) setUser(data.responseObject);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (!user) getUserData();
+    else setLoading(false);
+  }, []);
+  return loading ? null : user ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default AuthRequired;
