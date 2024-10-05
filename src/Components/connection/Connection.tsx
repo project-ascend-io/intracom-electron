f293b20eb82ccd046e1e26f106dc1a04ed42df23
bobbygrdn <bobbygrdn777@gmail.com>
2024-10-04 21:04:36 -0700

    fix: updates logic for basic nagivation and proper styling

--- src/Components/connection/Connection.tsx
+++ src/Components/connection/Connection.tsx
@@ -1,4 +1,5 @@
 import { Avatar } from "../avatar/Avatar";
+import { useNavigate } from "react-router-dom";
 
 interface User {
   _id: string;
@@ -28,12 +29,19 @@ const cutMessageLength = (lastMessage: string) => {
 };
 
 export const Connection: React.FC<Chat | null> = (chat) => {
+  // TODO: Update the onClick function to route to Conversation page and set the current selected conversation to the clicked conversation
+  const navigate = useNavigate();
+
+  const handleGoToConversation = () => {
+    navigate("/conversation");
+  };
+
   return (
     <section
-      className="flex flex-row justify-between my-4 mx-6 items-center"
-      onClick={null}
+      className="flex flex-row justify-between py-4 px-6 items-center hover:bg-[#D7E7EE] hover:cursor-pointer"
+      onClick={handleGoToConversation}
     >
-      <span className="flex flex-row items-center">
+      <span className="flex flex-row items-center select-none">
         <Avatar style={"w-12 h-12 rounded-[50%]"} user={null} />
         <span className="flex flex-col ml-6">
           <h3 className="font-semibold">{"Amy C."}</h3>
@@ -42,7 +50,7 @@ export const Connection: React.FC<Chat | null> = (chat) => {
           </p>
         </span>
       </span>
-      <time className="text-[#61788A] text-sm">{"3h"}</time>
+      <time className="text-[#61788A] text-sm select-none">{"3h"}</time>
     </section>
   );
 };
