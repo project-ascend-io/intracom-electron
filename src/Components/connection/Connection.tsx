01043f7e8263aede28e217c3a4177ecc0b743df6
bobbygrdn <bobbygrdn777@gmail.com>
2024-10-09 17:12:09 -0700

    fix: moves types to seperate file and updates naming conventions

--- src/Components/connection/Connection.tsx
+++ src/Components/connection/Connection.tsx
@@ -1,31 +1,15 @@
 import { Avatar } from "../avatar/Avatar";
 import { useNavigate } from "react-router-dom";
+import { Chat } from "../connection/Connection.types";
 
-interface User {
-  _id: string;
-  username: string;
-}
-
-interface Chat {
-  _id?: string;
-  chatName?: string;
-  isChannel?: boolean;
-  users?: User[];
-  chatAdmin?: User | null;
-  createdAt?: string;
-  updatedAt?: string;
-  lastMessage?: string;
-  __v?: number;
-}
-
-const cutMessageLength = (lastMessage: string) => {
+const cutMessageLength = (lastMessageText: string) => {
   const maxLength = 100;
 
-  if (lastMessage.length > maxLength) {
-    return lastMessage.slice(0, maxLength) + "...";
+  if (lastMessageText.length > maxLength) {
+    return lastMessageText.slice(0, maxLength) + "...";
   }
 
-  return lastMessage;
+  return lastMessageText;
 };
 
 export const Connection: React.FC<Chat | null> = (chat) => {
