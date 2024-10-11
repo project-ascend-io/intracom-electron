3f3f41d05322e79c48532d9080a31c9abb3b5d6f
bobbygrdn <bobbygrdn777@gmail.com>
2024-10-10 19:34:46 -0700

    fix: updates logic to use appropriate type check

--- src/Components/message/Message.tsx
+++ src/Components/message/Message.tsx
@@ -1,6 +1,6 @@
 import { Avatar } from "../avatar/Avatar";
 
-interface MessageProps {
+export type Message = {
   _id?: string;
   sender?: {
     _id?: string;
@@ -14,18 +14,20 @@ interface MessageProps {
   createdAt?: string;
   updatedAt?: string;
   __v?: number;
-}
+};
 
-export const Message: React.FC<MessageProps | null> = (props) => {
+export const Message: React.FC<Message> = (message) => {
   return (
     <section className="pl-2 flex items-start mb-2">
       <Avatar style={"w-10 h-10 rounded-[50%] object-cover mr-4"} user={null} />
       <div className="flex flex-col flex-1 items-start">
         <span className="flex justify-start">
-          <h6 className="mr-4 font-medium text-xs">{props.sender.username}</h6>
-          <time className="text-xs">{props.createdAt}</time>
+          <h6 className="mr-4 font-medium text-xs">
+            {message.sender.username}
+          </h6>
+          <time className="text-xs">{message.createdAt}</time>
         </span>
-        <p className="text-wrap text-xs mt-2">{props.content}</p>
+        <p className="text-wrap text-xs mt-2">{message.content}</p>
       </div>
     </section>
   );
