4092a87834a0c9b880fa52a4d0395bc89914507a
bobbygrdn <bobbygrdn777@gmail.com>
2024-10-11 20:19:31 -0700

    feat: creates logic to display the current user list and accomplish the functional requirements

---
+++ src/Components/userList/UserListItem.tsx
@@ -0,0 +1,30 @@
+import { User } from "../connection/Connection.types";
+import { Avatar } from "../avatar/Avatar";
+
+interface UserListItemProps {
+  user: User;
+  handleCreateConversation: (event: React.MouseEvent<HTMLElement>) => void;
+}
+
+export const UserListItem: React.FC<UserListItemProps> = ({
+  user,
+  handleCreateConversation,
+}) => {
+  return (
+    <li
+      className="flex flex-row justify-between py-4 px-6 items-center hover:bg-[#D7E7EE] hover:cursor-pointer rounded-lg"
+      onClick={handleCreateConversation}
+      id={user._id}
+    >
+      <span className="flex flex-row items-center select-none pointer-events-none">
+        <Avatar
+          style={"w-8 h-8 rounded-[50%] mr-4 select-none pointer-events-none"}
+          user={null}
+        />
+        <h6 className="font-semibold select-none pointer-events-none">
+          {user.username}
+        </h6>
+      </span>
+    </li>
+  );
+};
