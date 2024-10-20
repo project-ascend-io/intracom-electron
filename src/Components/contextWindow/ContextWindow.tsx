4b11b31a15f6ea07f6881c37f0a598f846738e09
bobbygrdn <bobbygrdn777@gmail.com>
2024-10-19 23:00:46 -0700

    fix: lifts socker provider to parent component

--- src/Components/contextWindow/ContextWindow.tsx
+++ src/Components/contextWindow/ContextWindow.tsx
@@ -4,21 +4,18 @@ import { DirectMessages } from "../../pages/directMessages/DirectMessages";
 import { NewDirectMessage } from "../../pages/newDirectMessage/NewDirectMessage";
 import { Conversation } from "../../pages/conversation/Conversation";
 import EmailConfiguration from "../../pages/emailConfigurations/email-configuration";
-import { SocketProvider } from "../../context/socketContext";
 
 export const ContextWindow = () => {
   return (
-    <SocketProvider>
-      <main className="flex h-full w-5/6 min-w-96">
-        <Routes>
-          <Route path="/" element={<Navigate to="/messages" />} />
-          <Route path="/messages" element={<DirectMessages />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/email-configuration" element={<EmailConfiguration />} />
-          <Route path="/new-direct-message" element={<NewDirectMessage />} />
-          <Route path="/conversation" element={<Conversation />} />
-        </Routes>
-      </main>
-    </SocketProvider>
+    <main className="flex h-full w-5/6 min-w-96">
+      <Routes>
+        <Route path="/" element={<Navigate to="/messages" />} />
+        <Route path="/messages" element={<DirectMessages />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/email-configuration" element={<EmailConfiguration />} />
+        <Route path="/new-direct-message" element={<NewDirectMessage />} />
+        <Route path="/conversation" element={<Conversation />} />
+      </Routes>
+    </main>
   );
 };
