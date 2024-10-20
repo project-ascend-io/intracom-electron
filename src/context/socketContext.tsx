ea5d93e52e348b8b83a1f4f814901e71e6f28058
bobbygrdn <bobbygrdn777@gmail.com>
2024-10-19 23:03:35 -0700

    feat: adds logic to consume socket events and update app data in real time

--- src/context/socketContext.tsx
+++ src/context/socketContext.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "./auth-context";
 import { useCurrentlySelectedChatContext } from "./currentlySelectedChatContext";
 import { useConversationsContext } from "./conversationsContext";
 import { useCurrentUsersContext } from "./currentUsersContext";
+import { useNavigate } from "react-router-dom";
 
 interface SocketContextType {
   socket: Socket | null;
@@ -25,14 +26,14 @@ export const SocketProvider: FC<{ children: React.ReactNode }> = ({
 }) => {
   const [socket, setSocket] = useState<Socket | null>(null);
 
+  const navigate = useNavigate();
+
   const { user } = useAuth();
-  const {
-    currentlySelectedChat,
-    setUserIsTypingInCurrentChat,
-    updateMessages,
-  } = useCurrentlySelectedChatContext();
-  const { updateCurrentConversations } = useConversationsContext();
-  const { updateCurrentUsers } = useCurrentUsersContext();
+  const { setCurrentlySelectedChat, setMessages } =
+    useCurrentlySelectedChatContext();
+  const { setConversations, setFilteredConversations } =
+    useConversationsContext();
+  const { setCurrentUsers, setFilteredCurrentUsers } = useCurrentUsersContext();
 
   useEffect(() => {
     const socketInstance = io(process.env.API_URL);
@@ -49,23 +50,47 @@ export const SocketProvider: FC<{ children: React.ReactNode }> = ({
       setSocket(null);
     });
 
-    socketInstance.on("chatUpdated", (chat) =>
-      updateCurrentConversations(chat),
+    socketInstance.on("chats change", (chat) => {
+      setFilteredConversations(null);
+      setConversations((prevConversations) => [...prevConversations, chat]);
+    });
+
+    socketInstance.on("chats delete", (deletedChatId) => {
+      setConversations((prevConversations) =>
+        prevConversations.filter(
+          (conversation) => conversation._id !== deletedChatId,
+        ),
+      );
+      const currentChatId = sessionStorage.getItem("currentlySelectedChat");
+      if (JSON.parse(currentChatId)._id === deletedChatId) {
+        setCurrentlySelectedChat(null);
+        navigate("/messages");
+      }
+    });
+
+    socketInstance.on("users change", (user) =>
+      setCurrentUsers((prevUsers) => [...prevUsers, user]),
     );
 
-    socketInstance.on("userUpdated", (user) => updateCurrentUsers(user));
-
-    socketInstance.on("message received", (message) => updateMessages(message));
-
-    socketInstance.on("typing", (roomId) => {
-      if (currentlySelectedChat._id === roomId)
-        setUserIsTypingInCurrentChat(true);
+    socketInstance.on("users delete", (deletedUserId) => {
+      setCurrentUsers((prevUsers) => {
+        return prevUsers.filter((user) => user._id !== deletedUserId);
+      });
+      setFilteredCurrentUsers(null);
     });
 
-    socketInstance.on("stop typing", (roomId) => {
-      if (currentlySelectedChat._id === roomId)
-        setUserIsTypingInCurrentChat(false);
+    socketInstance.on("messages change", (message) => {
+      const currentChatId = sessionStorage.getItem("currentlySelectedChat");
+      if (JSON.parse(currentChatId)._id === message.chat) {
+        setMessages((prevMessages) => [...prevMessages, message]);
+      }
     });
+
+    socketInstance.on("messages delete", (deletedMessageId) =>
+      setMessages((prevMessages) =>
+        prevMessages.filter((message) => message._id !== deletedMessageId),
+      ),
+    );
 
     return () => {
       socketInstance.disconnect();
